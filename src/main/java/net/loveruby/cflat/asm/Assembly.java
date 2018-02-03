a2ba0aa4a2282aef93a10d32613f37659e594e19
pkunewton <420218190@qq.com>
2018-02-03 18:19:01 +0800

    javacc 生成文件

---
+++ src/main/java/net/loveruby/cflat/asm/Assembly.java
@@ -0,0 +1,7 @@
+package net.loveruby.cflat.asm;
+
+/**
+ * Created by Administrator on 2018/1/30.
+ */
+abstract public class Assembly {
+}
---
+++ src/main/java/net/loveruby/cflat/sysdep/AssemblyCode.java
@@ -0,0 +1,7 @@
+package net.loveruby.cflat.sysdep;
+
+/**
+ * Created by Administrator on 2018/1/30.
+ */
+public interface AssemblyCode {
+}
